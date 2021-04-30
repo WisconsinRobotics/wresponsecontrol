5cdbe0edced626198fc854e3bd10f128c77f405f
Ben Nowotny <bennowotny65535@gmail.com>
2021-04-29 20:36:14 -0500

    Used private namespace for getting YAML config. variables and more advanced type-checking for YAML configurability.

--- src/WResponseControl.cpp
+++ src/WResponseControl.cpp
@@ -7,8 +7,10 @@ int main(int argc, char** argv){
     ros::init(argc, argv, "WResponseControl", ros::InitOption::AnonymousName);
 
     ros::NodeHandle n;
+    ros::NodeHandle np("~");
     XmlRpc::XmlRpcValue controllerParams;
-    n.getParam("controllers", controllerParams);
+    np.getParam("controllers", controllerParams);
+    ROS_INFO("ENUM TYPE: %d", controllerParams.getType());
     ROS_ASSERT(controllerParams.getType() == XmlRpc::XmlRpcValue::TypeArray);
     PIDController* controllers[controllerParams.size()]; 
     for(int i = 0; i < controllerParams.size(); i++){
@@ -29,14 +31,15 @@ int main(int argc, char** argv){
         ROS_ASSERT(controllerParams[i].hasMember("outputTopic") && controllerParams[i]["outputTopic"].getType() == XmlRpc::XmlRpcValue::TypeString);
         output = static_cast<std::string>(controllerParams[i]["outputTopic"]);
 
-        ROS_ASSERT(controllerParams[i].hasMember("P") && controllerParams[i]["P"].getType() == XmlRpc::XmlRpcValue::TypeDouble);
-        P = static_cast<double>(controllerParams[i]["P"]);
+        ROS_INFO("ENUM TYPE2: %d", controllerParams[i]["P"].getType());
+        ROS_ASSERT(controllerParams[i].hasMember("P") && controllerParams[i]["P"].getType() == XmlRpc::XmlRpcValue::TypeDouble || controllerParams[i]["P"].getType() == XmlRpc::XmlRpcValue::TypeInt);
+        P = controllerParams[i]["P"].getType() == XmlRpc::XmlRpcValue::TypeDouble ? static_cast<double>(controllerParams[i]["P"]) : static_cast<int>(controllerParams[i]["P"]);
 
-        ROS_ASSERT(controllerParams[i].hasMember("I") && controllerParams[i]["I"].getType() == XmlRpc::XmlRpcValue::TypeDouble);
-        I = static_cast<double>(controllerParams[i]["I"]);
+        ROS_ASSERT(controllerParams[i].hasMember("I") && controllerParams[i]["I"].getType() == XmlRpc::XmlRpcValue::TypeDouble || controllerParams[i]["I"].getType() == XmlRpc::XmlRpcValue::TypeInt);
+        I = controllerParams[i]["I"].getType() == XmlRpc::XmlRpcValue::TypeDouble ? static_cast<double>(controllerParams[i]["I"]) : static_cast<int>(controllerParams[i]["I"]);
 
-        ROS_ASSERT(controllerParams[i].hasMember("D") && controllerParams[i]["D"].getType() == XmlRpc::XmlRpcValue::TypeDouble);
-        D = static_cast<double>(controllerParams[i]["D"]);
+        ROS_ASSERT(controllerParams[i].hasMember("D") && controllerParams[i]["D"].getType() == XmlRpc::XmlRpcValue::TypeDouble || controllerParams[i]["D"].getType() == XmlRpc::XmlRpcValue::TypeInt);
+        D = controllerParams[i]["D"].getType() == XmlRpc::XmlRpcValue::TypeDouble ? static_cast<double>(controllerParams[i]["D"]) : static_cast<int>(controllerParams[i]["D"]);
 
         controllers[i] = new PIDController(setpoint, feedback, output, n);
         controllers[i]->setPID(P, I, D);
