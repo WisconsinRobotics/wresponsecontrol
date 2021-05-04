54205722835afcf3d4ed0e696392fb9965e8e016
Ben Nowotny <bennowotny65535@gmail.com>
2021-05-04 10:40:55 -0500

    Added file logging to get a better graphical representation of the PID testing, added a signal handler to close the file stream on closing the program.

--- src/WResponseTest.cpp
+++ src/WResponseTest.cpp
@@ -1,11 +1,20 @@
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <iostream>
 #include <std_msgs/Float64.h>
+#include <fstream>
+#include <csignal>
 
 double delta = 0;
 void deltaCallback(const std_msgs::Float64::ConstPtr& msg){
     delta = msg->data;
     ROS_INFO("PID_OUTPUT: %0.6f", delta);
+}
+
+std::fstream logFile;
+void closeFileHandleOnExit(int signal){
+    logFile.close();
+    exit(1);
 }
 
 int main(int argc, char** argv){
@@ -15,7 +24,14 @@ int main(int argc, char** argv){
     ros::Publisher feedbackPub = n.advertise<std_msgs::Float64>("/testFeedbackTopic0", 1000);
     ros::Subscriber deltaSub = n.subscribe("/testOutputTopic0", 1000, &deltaCallback);
 
-    int hz = 100;
+    // For whatever reason, this goes to ~/.ros/log.csv
+    logFile.open("log.csv", std::ios::out | std::ios::trunc);
+    ROS_INFO("FILE_OPEN: %s", logFile.is_open() ? "true" : "false");
+    ROS_INFO("FILE_OPEN2: %s", logFile ? "true" : "false");
+    if(logFile.is_open()) logFile << "setpoint,feedback,output" << std::endl;
+    signal(SIGINT, &closeFileHandleOnExit);
+
+    int hz = 50;
     ros::Rate loop(hz);
     bool setHigh = false;
     int setCounter = 0;
@@ -38,6 +54,14 @@ int main(int argc, char** argv){
         std_msgs::Float64 feedback;
         feedback.data = currOutput;
         feedbackPub.publish(feedback);
+
+        if(logFile.is_open()){
+            std::string log;
+            char buffer[300];
+            sprintf(buffer, "%0.6f,%0.6f,%0.6f", setpoint.data, feedback.data, delta);
+            log = buffer;
+            logFile << log << std::endl;
+        }
 
         ros::spinOnce();
         loop.sleep();
