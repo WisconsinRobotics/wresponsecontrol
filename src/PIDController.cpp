c5b5f5ee0a86881a33c610f28166fbe867df92d7
Ben Nowotny <bennowotny65535@gmail.com>
2021-05-10 19:56:51 -0500

    Added documentation to the PID Controller source file.

--- src/PIDController.cpp
+++ src/PIDController.cpp
@@ -1,12 +1,16 @@
 #include "PIDController.hpp"
 
 void PIDController::setPointCallback(const MsgPtr msg){
+    // Capture the setpoint message data
     this->setpoint = msg->data;
+    // Change the init state to reflect that a setpoint has been received
     this->initState |= 1;
 }
 
 void PIDController::feedbackCallback(const MsgPtr msg){
+    // Capture the feedback message data
     this->feedback = msg->data;
+    // Change the init state to reflect that a feedback message has been received
     this->initState |= 2;
 }
 
@@ -19,10 +23,12 @@ void PIDController::updateLastCalculationTime(){
 }
 
 PIDController::PIDController(std::string setPointTopic, std::string feedbackTopic, std::string outputTopic, ros::NodeHandle& node){
+    // Initialize the ROS topics for this controller
     this->setPointReader = node.subscribe(setPointTopic, 1000, &PIDController::setPointCallback, this);
     this->feedbackReader = node.subscribe(feedbackTopic, 1000, &PIDController::feedbackCallback, this);
     this->outputController = node.advertise<std_msgs::Float64>(outputTopic, 1000);
 
+    // Initialize all variables to 0 or false
     this->P = 0;
     this->I = 0;
     this->D = 0;
@@ -32,32 +38,42 @@ PIDController::PIDController(std::string setPointTopic, std::string feedbackTopi
     this->outputCapSet[0] = false;
     this->outputCapSet[1] = false;
     this->iCapSet = false;
+    // Set the last calculation time for the next calculation
     this->updateLastCalculationTime();
 }
 
 double PIDController::computeNextOutput(){
+    // Store the current error as the last error
     this->lastErr = this->err;
+    // Compute the new current error
     this->err = this->setpoint - this->feedback;
+    // Add the new current error to the summed error
     this->sumErr += this->err;
+    // Cap the summed error if the summed error cap is set
     this->sumErr = this->getICapSet() && abs(this->sumErr) > abs(this->getICap()) ? this->getICap() * this->sumErr/abs(this->sumErr) : this->sumErr;
+    // Calculate the time elapsed since the last calculation in seconds
     double timeElapsed = (PIDController::getCurrentTime().count() - this->lastCalculationTime.count())/1000.f;
-    // ROS_INFO("TIME ELAPSED: %0.6f", timeElapsed*1000);
+    // Use the standard PID formula to compute the next output
     this->lastOutput = this->getP()*this->err + this->getI()*this->sumErr*timeElapsed + this->getD()*(this->err - this->lastErr)/timeElapsed;
+    // Cap the next output if applicable
     this->lastOutput = this->getMaxOutputSet() && this->lastOutput > this->getMaxOutput() ? this->getMaxOutput() : this->getMinOutputSet() && this->lastOutput < this->getMinOutput() ? this->getMinOutput() : this->lastOutput;
     return this->lastOutput;
 }
 
 void PIDController::computeAndSendNextOutput(){
+    // If both a setpoint and feedback message have been received...
     if(this->initState == 3){
+        // Compute and send the next output to the output ROS topic
         std_msgs::Float64 msgNext;
         msgNext.data = this->computeNextOutput();
-        // ROS_INFO("PID_CONTROLLER_CHECK: %0.6f", msgNext.data);
         this->outputController.publish(msgNext);
     }
+    // Set the last calculation time to provide for the correct next output calculation
     this->updateLastCalculationTime();
 }
 
 void PIDController::executeNextControlCycle(){
+    // This method does the control cycle work
     this->computeAndSendNextOutput();
 }
 
@@ -93,20 +109,24 @@ void PIDController::setPID(double P, double I, double D){
 
 void PIDController::setMaxOutput(double max){
     this->outputCap[1] = max;
+    // Set that the max ouput value has been set
     this->outputCapSet[1] = true;
 }
 
 double PIDController::getMaxOutput(){
+    // If the max output is not set, throw an error
     if(this->getMaxOutputSet()) return this->outputCap[1];
     throw "The max value of this controller is not set";
 }
 
 void PIDController::setMinOutput(double min){
     this->outputCap[0] = min;
+    // Set that the min output value has been set
     this->outputCapSet[0] = true;
 }
         
 double PIDController::getMinOutput(){
+    // If the min output is not set, throw an error
     if(this->getMinOutputSet()) return this->outputCap[0];
     throw "The min value of this controller is not set";
 }
@@ -126,10 +146,12 @@ bool PIDController::getMinOutputSet(){
 
 void PIDController::setICap(double iCap){
     this->iCap = iCap;
+    // Set that the summed error cap is set
     this->iCapSet = true;
 }
 
 double PIDController::getICap(){
+    // If the summed error cap is not set, throw an error
     if(this->getICapSet()) return this->iCap;
     throw "The I Cap of this controller is not set";
 }
